f9f48b433f1f9c02e38e91c90841bdef2a25589b
Clarice Bouwer <cbillowes@gmail.com>
2016-04-26 19:22:53 +0200

    Add bootstrap sass with sass grunt compilation task to compile and output css to _build folder which is ignored

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,31 @@ module.exports = function(grunt) {
         serve: {
           command: 'jekyll serve --skip-initial-build'
         }
+      },
+
+      sass: {
+        bootstrap: {
+          options: {
+            style: 'compressed',
+            noCache: true,
+            loadPath: 'node_modules/bootstrap-sass/assets/stylesheets'
+          },
+          files: {
+              // Source file fails to generate output when starting with _.
+              // destination : source
+              '_build/css/bootstrap.min.css': '_sass/bootstrap-override.scss'
+          }
+        }
+      },
       }
     });
 
     grunt.registerTask('build', ['shell:build']);
+    grunt.registerTask('compile-sass', ['sass:bootstrap']);
 
     grunt.registerTask('serve', ['shell:serve']);
 
     grunt.registerTask('default', ['build', 'serve']);
 
+    //execute `grunt %_TASK_% -v` for verbose messaging
 };
