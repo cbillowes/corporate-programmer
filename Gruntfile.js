b33115b60546a8230b0099df137cc9d990102e5b
Clarice Bouwer <cbillowes@gmail.com>
2016-04-27 17:09:22 +0200

    Rename main.scss to essentials to indicate essential styles for site to be placed in this file compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
             sourcemap: 'none'
           },
           files: {
-            'css/main.min.css' : '_sass/main.scss'
+            'css/essentials.min.css' : '_sass/essentials.scss'
           }
         }
       },
