1e3b83c49c32f29e9fb6706b2050f4b487904841
Clarice Bouwer <cbillowes@gmail.com>
2016-05-11 23:19:31 +0200

    Add space in script.sql

--- app/script.sql
+++ app/script.sql
@@ -3,5 +3,5 @@
 use corposq0_database;
 
 {% for post in site.posts %}
-INSERT INTO posts (permalink, title, date_time, full_text) VALUES ('{{ post.url }}', '{{ post.title | titlecase | replace: "'", "''" }}', '{{ post.date | date: "%Y-%m-%d %T"  }}', '{{ post.content | strip_html | downcase| full_text_search }}');
+INSERT INTO posts (permalink, title, date_time, full_text) VALUES ('{{ post.url }}', '{{ post.title | titlecase | replace: "'", "''" }}', '{{ post.date | date: "%Y-%m-%d %T"  }}', '{{ post.content | strip_html | downcase | full_text_search }}');
 {% endfor %}
