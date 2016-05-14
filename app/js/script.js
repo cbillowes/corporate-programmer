5af9c63b323f857173d4d24e9be708c771292f9b
Clarice Bouwer <cbillowes@gmail.com>
2016-05-14 22:05:56 +0200

    Make footer stick to bottom of page

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,13 @@
 $(function() {
 
+  if ($(document).height() <= $(window).height()) {
+    $('body > footer').css({ 'position': 'fixed', 'left': '0px', 'bottom': '0px', 'right': '0px' });
+
+    $(window).scroll(function() {
+      $('body > footer').css({ 'position': 'relative' });
+    });
+  }
+
   $('#more-tags').click(function() {
     $('#tags').removeClass('hidden');
     $('body > footer').css({ 'margin-bottom': $('#tags').outerHeight() - 10 });
