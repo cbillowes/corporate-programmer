3d3afde4bace17e762f21f10cffb9b379f28a501
Clarice Bouwer <cbillowes@gmail.com>
2016-06-19 10:40:15 +0200

    Feature: Add navigation bar for posts
    Users can now browse through h2 content using a navigation bar that appears at the bottom of the page

--- app/_assets/js/script.js
+++ app/_assets/js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+  var hasNavbarForPosts = $('#navbar-post').size() > 0;
 
   if ($(document).height() <= $(window).height()) {
     $('body > footer').css({ 'position': 'fixed', 'left': '0px', 'bottom': '0px', 'right': '0px' });
@@ -8,16 +9,16 @@ $(function() {
     });
   }
 
-  if ($(document).height() - $(window).height() > 250) {
-    $(window).scroll(function() {
-      if ($(window).scrollTop() > $('body > header.top').outerHeight() / 2) {
-        $('body > header.top').addClass('fixed-for-scroll');
-      } else {
-        $('body > header.top').removeClass('fixed-for-scroll');
-      }
-    });
-  }
-  
+  $(window).scroll(function() {
+    positionMenu();
+    positionNavbarForPosts();
+  });
+
+  $(window).resize(function() {
+    positionMenu();
+    positionNavbarForPosts();
+  });
+
   $('#more-tags').click(function() {
     $('#tags').removeClass('hidden');
     $('body > footer').css({ 'margin-bottom': $('#tags').outerHeight() - 10 });
@@ -31,5 +32,49 @@ $(function() {
   $(window).load(function() {
     $(window).scrollTop($(window).scrollTop() - $('h2').outerHeight() - 25);
   });
+
+  resizeImages();
+
+  if (hasNavbarForPosts) {
+    var list = '<ul class="nav">';
+    $('.post h2').each(function() {
+      var text = $(this).text();
+      var link = encodeURIComponent(text.toLowerCase().replace(/ /g, '-'));
+      list += '<li><a href="#' + link + '">' + text + '</a></li>';
+    });
+    $('#navbar-post').html(list + '</ul>');
+  }
+
+  function resizeImages() {
+    $('img').filter(function(){
+      return ($(this).width() > $(window).width())
+    }).css({ 'width': '100%'});
+  }
+
+  function positionMenu() {
+    if ($(document).height() - $(window).height() > 250) {
+      var $topHeader = $('body > header.top');
+
+      if ($(window).scrollTop() > $topHeader.outerHeight() / 2) {
+        if (!$topHeader.hasClass('fixed-for-scroll')) $topHeader.addClass('fixed-for-scroll');
+      } else {
+        if ($topHeader.hasClass('fixed-for-scroll')) $topHeader.removeClass('fixed-for-scroll');
+      }
+    }
+  }
+
+  function positionNavbarForPosts() {
+    var $navbarPost = $('#navbar-post');
+    if (hasNavbarForPosts) {
+      if ($(window).scrollTop() > $('.hero').outerHeight()) {
+        var offset = $(window).scrollTop() + $(window).height() - $navbarPost.outerHeight() + 1;
+        if (offset > $('footer.site').offset().top) {
+          $navbarPost.css({ 'bottom': -$navbarPost.outerHeight() });
+        } else {
+          $navbarPost.css({ 'bottom': '0px' });
+        }
+      }
+    }
+  }
 
 });
